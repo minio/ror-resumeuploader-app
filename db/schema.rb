51c760c7343b2f499ee8a4816ec8877ce13558d9
Dee Koder <deekoder@users.noreply.github.com>
2016-06-07 00:52:10 -0700

    Added config/secrets.yml file to be compatible with latest version of rails. It seems to work backwards as well. Remove unecessary table and commentout secrets.yml out of gitignore (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160316223541) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "uploads", force: :cascade do |t|
     t.string   "url"
     t.string   "name"
